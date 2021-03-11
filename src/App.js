406b16bf2f069a3c83eeb433e6ffe5c8c160998b
BojanIstvancic <bojan.istvancic@gmail.com>
2021-03-11 15:43:27 +0100

    Add React-Router to application

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import AboutUs from "./pages/AboutUs";
 import Nav from "./components/Nav";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
 // Global Style
 import GlobalStyle from "./components/GlobalStyle";
+// Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          {/* exact - render this route if the path is EXACTLY THE SAME - not /work or /contact */}
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
+      {/* Switch renders just one route per page - first one that matches the URL */}
     </div>
   );
 }
