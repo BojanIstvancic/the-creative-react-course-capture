a0c4406184af9c9e76dbcd5a492bfcfcf068bb91
BojanIstvancic <bojan.istvancic@gmail.com>
2021-03-11 13:24:32 +0100

    4. Faq Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,14 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
       <ServicesSection />
+      <FaqSection />
       {/* if we don't want a div we can create fraction <></> - it is mimicking divs */}
     </>
   );
