a554358255e36ad07f2fe48dc28a84dc3b9a2599
BojanIstvancic <bojan.istvancic@gmail.com>
2021-03-12 11:14:01 +0100

    12.  Framer Motion - practice

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,29 +1,31 @@
 import React from "react";
 import home1 from "../img/home1.png";
 import { Description, Layout, Image, Hide } from "../styles";
+// Framer Motion
+import { motion } from "framer-motion";
 
 const AboutSection = () => {
   return (
     <Layout>
       <Description>
-        <div className="title">
+        <motion.div>
           <Hide>
-            <h2>We work to make</h2>
+            <motion.h2>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <h2>
+            <motion.h2>
               your <span>dreams</span> come
-            </h2>
+            </motion.h2>
           </Hide>
           <Hide>
-            <h2>true.</h2>
+            <motion.h2>true.</motion.h2>
           </Hide>
           <p>
             Contact us for any photography or videography ideas that you have.
             We have professionals with amaizing skills.
           </p>
           <button>Contact Us</button>
-        </div>
+        </motion.div>
       </Description>
       <Image>
         <img src={home1} alt="guy with the camera" />
